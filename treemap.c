0bb002d2815476ccaedb90d76dd4db611f7cc721
Your Name <you@example.com>
2024-05-13 03:26:16 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -148,8 +148,6 @@ void removeNode(TreeMap * tree, TreeNode* node)
             }
         }
     }
-    free(node->pair->key);
-    free(node->pair->value);
     free(node->pair);
     free(node);
 }
